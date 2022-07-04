5a114d110772ccca5213a0e8eba30534dda24ada
unknown <gowtham>
2022-07-04 22:06:05 +0530

    chnages for deployment

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,5 +1,6 @@
 const Api_Env = {
-    API_URL:"https://notegolangapp.herokuapp.com/"
+    API_URL:"https://notegolangapp.herokuapp.com/",
+    // API_URL:"http://localhost:8200/"
 }
 
 export default Api_Env
